ad9fe7b86e38c1c8d4dd699db957472c087f5b53
vickyleu <liuyizhi@xuelipai.cn>
2022-07-05 20:18:33 +0800

    wake on lan completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+// ignore_for_file: prefer_function_declarations_over_variables
+
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:dart_ping/dart_ping.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:wake_on_lan/wake_on_lan.dart';
 
 void main() {
+  //Wake on lan 网卡启动
+  //bios 找到wake on lan打开, 没有此项的话,如技嘉主板 开启 "平台电力管理",关闭"ErP",找到"IO PORTS",进去"Network Stack Configuration",打开PXE支持
+  // 进入windows ,进入控制面板,电源选项,关闭"快速启动",右键托盘网络图标,进入适配器管理界面,右键属性,网卡高级 电源管理 取消"允许计算机关闭此设备以节约电源",
+  // 否则电源管理还是会关闭网卡供电,勾选"允许此设备唤醒计算机"和"只允许幻数据包唤醒计算机"
   runApp(const MyApp());
 }
 
@@ -51,14 +59,60 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+typedef Callback = Function(PingSummary summary);
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  Future<void> startPing(String host,
+      {required int pingCount, Callback? callback}) async {
+    final ping = Ping(host,
+        encoding: const Utf8Codec(allowMalformed: true), count: pingCount);
+    print("command::${ping.command}");
+    ping.stream.listen((event) async {
+      if (event.error != null && event.summary == null) {
+        print("event :: ${event.error}");
+      } else {
+        if (event.response != null) {
+          if (event.response is PingSummary) {
+            final summary = event.response as PingSummary;
+            print("event summary:: ${summary}");
+            callback?.call(summary);
+          } else if (event.response is PingResponse) {
+            final ping = event.response as PingResponse;
+            ping.ip;
+            ping.ttl;
+            ping.time;
+          } else {
+            throw Exception("不想说了");
+          }
+        } else if (event.summary != null) {
+          final summary = event.summary as PingSummary;
+          callback?.call(summary);
+        }
+      }
+    });
+  }
+
   Future<void> _incrementCounter() async {
-    String mac = '4c:20:b8:eb:1d:42';
+
+    // 在命令提示符处键入“shutdown -m \ [IP 地址] -r -f”（不带引号），其中“[IP 地址]”是您要重新启动的计算机的 IP。例如，
+    // 如果您要重新启动的计算机位于 192.168.0.34，请键入“shutdown -m \ 192.168.0.34 -r -f”。“-r”和“-f”标志分别告诉远程计算机重新启动和安全关闭所有打开的程序。
+    // 按“Enter”确认命令并重新启动远程机器。
+
+    //shutdown -m \ 192.168.1.113 -r -f
+
+
+    String mac = 'D8:5E:D3:25:B0:5E'; //#  hongju
+    String ipv4 = '192.168.1.103'; //#  LAPTOP
+    // String mac = '5E:EE:75:CA:86:88'; //#  LAPTOP
+    // String ipv4 = '192.168.1.220';//#  LAPTOP
+
+    // String mac = '4c:20:b8:eb:1d:42';
     // String mac = 'd8:5e:d3:25:b0:5e';
     // String ipv4 = '192.168.1.168';
-    String ipv4 = '192.168.1.20';
+
+    // String ipv4 = '192.168.1.20';
     // String ipv4 = '292.268.221.99';
     // String ipv4 = '192.168.1.103';
     // if(MACAddress.validate(mac) && IPv4Address.validate(ipv4)) {
@@ -74,58 +128,47 @@ class _MyHomePageState extends State<MyHomePage> {
     // // const Utf8Codec(allowMalformed: true).decodeStream(p.stdout).then((value){
     // //   print("stdout:::${value}");
     // // });
-
-
     final pingCount = 3;
-    final ping = Ping(ipv4,
-        encoding: const Utf8Codec(allowMalformed: true),
-        count: pingCount
-    );
-    print("command::${ping.command}");
 
-    ping.stream.listen((event)  async {
-      if(event.response!=null){
-        if(event.response is PingSummary){
-          final summary = event.response as PingSummary;
-          if(summary.errors.length==pingCount){
-            //TODO 当前一个PING 都接收不到,肯定是关机或睡眠了, WOL来一下
-            if(MACAddress.validate(mac) && IPv4Address.validate(ipv4)) {
-              MACAddress macAddress = MACAddress(mac);
-              IPv4Address ipv4Address = IPv4Address(ipv4);
-              WakeOnLAN wol = WakeOnLAN(ipv4Address, macAddress);
-              await wol.wake().then((_) => print('sent'));
-            }
-            // chromium
-          }else{
-            if(summary.transmitted == summary.received){//TODO 完整接收了和返回了,正常开机状态
-
-            }else{//当前可以连通,但是有丢包,我也不知道干啥
-
-            }
-          }
+    final Callback callback = (summary) async {
+      print("event :: ${summary}");
+      if (summary.errors.length == pingCount ||
+          (summary.received == 0 && summary.transmitted > 0)) {
+        print("嗨呀,ping不通,需要唤醒机器!!");
+        //TODO 当前一个PING 都接收不到,肯定是关机或睡眠了, WOL来一下
+        if (MACAddress.validate(mac) && IPv4Address.validate(ipv4)) {
+          MACAddress macAddress = MACAddress(mac);
+          IPv4Address ipv4Address = IPv4Address(ipv4);
+          WakeOnLAN wol = WakeOnLAN(ipv4Address, macAddress, port: 7);
+          await wol.wake().then((_) => print('sent'));
+          Future.delayed(Duration(seconds: 20)).then((value) async {
+            await startPing(ipv4, pingCount: pingCount);
+          });
         }
-        else if(event.response is PingResponse){
-          final ping = event.response as PingResponse;
-          ping.ip;
-          ping.ttl;
-          ping.time;
-        }else{
-          throw Exception("不想说了");
+        // chromium
+      }
+      else {
+        if (summary.transmitted == summary.received) {
+          //TODO 完整接收了和返回了,正常开机状态
+          print("当前${ipv4}电脑是开机状态!准备关机");
+          // shutdown -m \ 192.168.1.113 -f
+          // shutdown -m \ 192.168.1.113 -r -f
+          final p=await Process.start(
+            'shutdown',
+            ["-m ", '\\', ipv4, '-f' ],
+            // environment: {'LANG': 'en_US'},
+          );
+
+          const Utf8Codec(allowMalformed: true).decodeStream(p.stdout).then((value){
+            print("stdout:::${value}");
+          });
+        } else {
+          //当前可以连通,但是有丢包,我也不知道干啥
+
         }
       }
-    });
-
-    // }
-
-
-    // setState(() {
-    //   // This call to setState tells the Flutter framework that something has
-    //   // changed in this State, which causes it to rerun the build method below
-    //   // so that the display can reflect the updated values. If we changed
-    //   // _counter without calling setState(), then the build method would not be
-    //   // called again, and so nothing would appear to happen.
-    //   _counter++;
-    // });
+    };
+    await startPing(ipv4, pingCount: pingCount, callback: callback);
   }
 
   @override
